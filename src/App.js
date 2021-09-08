fc22c15a04c58af8cca5f2e82e464513b98d550b
iCode-Cat <devranboyac8@gmail.com>
2021-09-08 22:23:55 +0300

    Application Wrapper

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <iframe title='header' scrolling='no' id='header' width='100%' height='100px'></iframe>
+      <iframe title='calculator' id='calculator' src='https://kind-shockley-bb822e.netlify.app/' width='100%' height='100%'></iframe>
+      <iframe title='footer' scrolling='no' id='footer' width='100%' height='100px'></iframe>
+    </div>
+  );
+}
+
+export default App;
